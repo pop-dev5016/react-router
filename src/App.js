cb15d563e7c1786f61c1e7a03dcc2c943a184af5
devpop <impopcity@gmail.com>
2022-10-04 12:34:12 +0530

    Adding Search state,post,serch,pass props2nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from './Header';
 import {Route,Switch,useHistory} from 'react-router-dom'
-import {usestate,useEffect} from 'react'
+import {useState,useEffect} from 'react'
 import Nav from './Nav';
 import NewPost from './NewPost';
 import PostPage from './PostPage';
@@ -10,10 +10,38 @@ import Footer from './Footer';
 import Home from './Home'
 
 function App() {
+  const [search,setSearch] = useState("")
+  const [searchResults,setSearchResults] = useState([])
+  const [posts,setPosts] = useState([
+    {
+      id: 1,
+      title: "My First Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 2,
+      title: "My 2nd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 3,
+      title: "My 3rd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 4,
+      title: "My Fourth Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    }
+  ])
   return (
     <div className="App">
       <Header title={"React Js Blog"}></Header>
-      <Nav></Nav>
+      <Nav search={search} setSearch={setSearch}></Nav>
 
       <Switch>
 
