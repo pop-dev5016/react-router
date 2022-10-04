1afa016c39fd5f9dfe4006f9a3d469f39138d909
devpop <impopcity@gmail.com>
2022-10-04 11:51:32 +0530

    setup to route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
+import {Route,Switch,useHistory} from 'react-router-dom'
+import {usestate,useEffect} from 'react'
+import Nav from './Nav';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import Missing from './Missing';
+import About from './About';
+import Footer from './Footer';
+import Home from './Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Nav></Nav>
+
+      <Switch>
+
+          <Route exact  path="/">
+            <Home/>
+          </Route>
+             
+          <Route exact path="/post">
+              <NewPost/>
+          </Route>
+
+          <Route path="/post/:id">
+              <PostPage/>
+          </Route>
+          
+          <Route path="/about" component={About}/>
+
+          <Route path= "*" component={Missing}/>
+
+      </Switch>
+      
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <main>
+        <h1> footer </h1>
+    </main>
+
+  )
+}
+
+export default Footer
