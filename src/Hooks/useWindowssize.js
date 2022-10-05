80e3db6e34e17a5a844cc0712af98ce372b9b425
devpop <impopcity@gmail.com>
2022-10-05 18:46:53 +0530

    create windows coustom hooks

---
+++ src/Hooks/useWindowssize.js
@@ -0,0 +1,32 @@
+import { useState,useEffect } from "react";
+
+import React from 'react'
+
+const useWindowssize = () => {
+    const [windowssize,setWindowsSize] = useState({width:undefined,height:undefined})
+    
+
+    useEffect(()=>{
+        
+      const handleResize = ()=>{
+        setWindowsSize({
+            width:innerWidth,
+            height:window.innerHeight
+        })
+      }
+       
+      handleResize()
+
+      window.addEventListener("resize",handleResize)
+
+      const cleanUp = ()=>{
+        console.log("runs if a useeffect dep changes");
+        window.removeEventListener("resize",handleResize)
+      }
+      return cleanUp
+    },[])
+
+    return windowssize
+}
+
+export default useWindowssize
